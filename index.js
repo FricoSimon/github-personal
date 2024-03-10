aabaa488eaef29acaa9d880814f7ec20e495637a
Frico Simon <simonfriko@gmail.com>
2024-03-10 14:31:17 +0700

    create index.js (BE)

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+const cors = require('cors');
+const axios = require('axios');
+const path = require('path');
+
+app.use(cors());
+
+// accept json and url encoded values
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/v1/followers', async (req, res) => {
+    const username = req.body.username;
+
+    axios.get(`https://api.github.com/users/${username}/followers?per_page=100`)
+        .then(response => {
+            const followers = response.data.map(follower => follower.login);
+            res.json(followers);
+        })
+        .catch(error => {
+            if (error.response.status === 404) {
+                res.status(404).json({ message: 'Username not found' });
+                console.log("Username not found");
+            } else {
+                console.log(error);
+            }
+        });
+});
+
+app.post('/api/v1/following', async (req, res) => {
+    const username = req.body.username;
+
+    axios.get(`https://api.github.com/users/${username}/following?per_page=100`)
+        .then(response => {
+            const following = response.data.map(following => following.login);
+            res.json(following);
+        })
+        .catch(error => {
+            if (error.response.status === 404) {
+                res.status(404).json({ message: 'Username not found' });
+                console.log("Username not found");
+            } else {
+                console.log(error);
+            }
+        });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
